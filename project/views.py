5e8db22cf70a856cf9d0f14541e3ac6fa01309fb
virendrapatel123 <virendrap9569@gmail.com>
2024-08-21 15:14:57 +0530

    commit

---
+++ project/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    result=0
+    data=dict({})
+
+   
+    if request.method=="POST":
+        n1=int(request.POST.get('num1'))
+        n2=int(request.POST.get('num2'))
+        result=n1+n2
+        data={
+            "a1":n1,
+            "a2":n2,
+            "output":result}
+
+    # print(sum)
+    return render(request,"index.html",data)
